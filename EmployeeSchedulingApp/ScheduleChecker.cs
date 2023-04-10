5134fefd1aed6d9771d6f4959aefcde900f26a58
gabrieljaooo5 <joaogabriel1598@gmail.com>
2023-04-10 14:38:29 -0300

    Create Schedule checker and tests to check to count coincidences

---
+++ EmployeeSchedulingApp/ScheduleChecker.cs
@@ -0,0 +1,31 @@
+﻿public static class ScheduleChecker
+{
+    public static int CountCoincidences(Employee employee1, Employee employee2)
+    {
+        int coincidences = 0;
+
+        foreach (var timeRange1 in employee1.Schedule)
+        {
+            foreach (var timeRange2 in employee2.Schedule)
+            {
+                if (timeRange1.DayOfWeek == timeRange2.DayOfWeek &&
+                    CheckTimeRangeCoincidence(timeRange1, timeRange2))
+                {
+                    coincidences++;
+                }
+            }
+        }
+
+        return coincidences;
+    }
+
+    private static bool CheckTimeRangeCoincidence(TimeRange timeRange1, TimeRange timeRange2)
+    {
+        if (timeRange1.EndTime <= timeRange2.StartTime || timeRange1.StartTime >= timeRange2.EndTime)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
--- EmployeeSchedulingTest/UnitTest1.cs
+++ EmployeeSchedulingTest/UnitTest1.cs
@@ -42,6 +42,44 @@ namespace EmployeeAppSchedulingTest
             Assert.IsNotNull(employees);
             Assert.AreEqual(3, employees.Count);
         }
+
+        [TestMethod]
+        public void Test_CountCoincidences_ReturnsCorrectResult()
+        {
+            // Arrange
+            Employee rene = new Employee("RENE", new List<TimeRange>()
+            {
+                new TimeRange(DayOfWeek.Monday, new TimeSpan(10, 0, 0), new TimeSpan(12, 0, 0)),
+                new TimeRange(DayOfWeek.Tuesday, new TimeSpan(10, 0, 0), new TimeSpan(12, 0, 0)),
+                new TimeRange(DayOfWeek.Thursday, new TimeSpan(1, 0, 0), new TimeSpan(3, 0, 0)),
+                new TimeRange(DayOfWeek.Saturday, new TimeSpan(14, 0, 0), new TimeSpan(18, 0, 0)),
+                new TimeRange(DayOfWeek.Sunday, new TimeSpan(20, 0, 0), new TimeSpan(21, 0, 0))
+            });
+
+            Employee astrid = new Employee("ASTRID", new List<TimeRange>()
+            {
+                new TimeRange(DayOfWeek.Monday, new TimeSpan(10, 0, 0), new TimeSpan(12, 0, 0)),
+                new TimeRange(DayOfWeek.Thursday, new TimeSpan(12, 0, 0), new TimeSpan(14, 0, 0)),
+                new TimeRange(DayOfWeek.Sunday, new TimeSpan(20, 0, 0), new TimeSpan(21, 0, 0))
+            });
+
+            Employee andres = new Employee("ANDRES", new List<TimeRange>()
+            {
+                new TimeRange(DayOfWeek.Monday, new TimeSpan(10, 0, 0), new TimeSpan(12, 0, 0)),
+                new TimeRange(DayOfWeek.Thursday, new TimeSpan(12, 0, 0), new TimeSpan(14, 0, 0)),
+                new TimeRange(DayOfWeek.Sunday, new TimeSpan(20, 0, 0), new TimeSpan(21, 0, 0))
+            });
+
+            // Act
+            int astridReneCoincidences = ScheduleChecker.CountCoincidences(astrid, rene);
+            int astridAndresCoincidences = ScheduleChecker.CountCoincidences(astrid, andres);
+            int reneAndresCoincidences = ScheduleChecker.CountCoincidences(rene, andres);
+
+            // Assert
+            Assert.AreEqual(2, astridReneCoincidences);
+            Assert.AreEqual(3, astridAndresCoincidences);
+            Assert.AreEqual(2, reneAndresCoincidences);
+        }
     }
 }
 
